d6f0f0cac0f8159e58d7ff5825d1bc942767bef5
Denali Balser <denali.balser167@gmail.com>
2019-05-23 15:39:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,9 +4,9 @@ class Song
   @@count = 0
 
   def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre
+    @@name = name
+    @@artist = artist
+    @@genre = genre
     @@count += 1
     @@genres << genre
   end
