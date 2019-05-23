f21c076b8597323bd874dcb44cf5b1d03f9a8c00
Denali Balser <denali.balser167@gmail.com>
2019-05-23 15:44:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,9 +4,16 @@ class Song
   @@count = 0
 
   def initialize(name, artist, genre)
-    @@count += 1
+    @name = name
+    @artist = artist
+    @@artists << artist
+    @genre = genre
     @@genres << genre
+    @@count += 1
   end
+  attr_accessor :name, :artist, :genre
+end
+
 
   def self.count
     @@count
