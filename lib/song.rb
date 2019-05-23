828b97e682efcfc5c5eeffa52d0b583f1682bd86
Denali Balser <denali.balser167@gmail.com>
2019-05-23 16:08:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,6 +28,18 @@ class Song
     @@artists.uniq
   end
 
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+      genre_count[genre] += 1
+      else
+      genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+
   def self.artist_count
     artist_count = {}
     @@artists.each do |artist|
